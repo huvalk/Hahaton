221b8742f9a7fd04ad2a1bad8d3ccd4951f80486
Куклин С. И <akiraq@yandex.ru>
2020-11-09 02:41:02 +0300

    fix: workflow

--- cmd/build_opencv.sh
+++ cmd/build_opencv.sh
@@ -24,76 +24,21 @@ mv opencv_contrib-4.4.0 opencv_contrib
 
 echo "DK-INFO: Install OpenCV build dependencies"
 
-apt update && apt upgrade -y
-apt-get dist-upgrade -y --autoremove
-
-apt install -y \
-        build-essential \
-        cmake \
-        git \
-        pkg-config
-
-apt install -y \
-    gfortran \
-    libatlas-base-dev \
-    liblapacke-dev \
-    liblapack-dev \
-    libavcodec-dev \
-    libavformat-dev \
-    libswscale-dev \
-    libv4l-dev \
-    v4l-utils \
-    libxvidcore-dev \
-    libx264-dev \
-    libfontconfig1-dev \
-    libcairo2-dev \
-    libgdk-pixbuf2.0-dev \
-    libpango1.0-dev \
-    libgtk2.0-dev \
-    libgtk-3-dev \
-    libcanberra-gtk3* \
-    libhdf5-dev \
-    libhdf5-serial-dev \
-    libqtgui4 \
-    libqtwebkit4 \
-    libqt4-test \
-    python3-pyqt5 \
-        libgstreamer1.0-dev \
-        libgstreamer-plugins-base1.0-dev \
-    libjpeg-dev \
-    libtiff-dev \
-    libpng-dev \
-    libjasper-dev \
-    libtbb-dev \
-    libtbb2 \
-    python3-dev \
-    python3-pip \
-    python3-numpy
-
-
-
 CMAKEFLAGS="
     -D CMAKE_BUILD_TYPE=RELEASE
     -D CMAKE_INSTALL_PREFIX=/usr/local
     -D OPENCV_EXTRA_MODULES_PATH=~/opencv_contrib/modules
     -D BUILD_EXAMPLES=OFF
+    -D WITH_QT=OFF
     -D BUILD_TESTS=OFF
     -D BUILD_PERF_TESTS=OFF
     -D INSTALL_PYTHON_EXAMPLES=OFF
     -D INSTALL_C_EXAMPLES=OFF
-    -D ENABLE_NEON=ON
-    -D ENABLE_VFPV3=ON
-    -D OPENCV_ENABLE_NONFREE=ON
-    -D CMAKE_SHARED_LINKER_FLAGS=-latomic
-    -D WITH_FFMPEG=ON
+    -D WITH_OPENGL=ON
+    -D FORCE_VTK=ON
     -D WITH_TBB=ON
-    -D BUILD_TBB=ON
-    -D WITH_GSTREAMER=ON
-    -D WITH_V4L=ON
-    -D WITH_LIBV4L=ON
-    -D BUILD_NEW_PYTHON_SUPPORT=ON
-    -D ENABLE_PRECOMPILED_HEADERS=ON
-    -D OPENCV_GENERATE_PKGCONFIG=ON"
+    -D WITH_GDAL=ON
+    -D WITH_XINE=ON"
 
 
 echo "DK-INFO>cmake flags: ${CMAKEFLAGS}"
